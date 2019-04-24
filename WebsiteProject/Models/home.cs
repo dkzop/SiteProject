8250a0442f5f260156497efb9062d572d989b30d
joaoborges96 <jsousaborges@hotmail.com>
2019-04-24 09:25:41 +0100

    Homepae Images done

--- WebsiteProject/Models/home.cs
+++ WebsiteProject/Models/home.cs
@@ -17,27 +17,129 @@ namespace WebsiteProject.Models
 {
     public class Home : ContentModel
     {
-        public string Title
+        public string HomeTitle
         {
-            get { return Content.Value<string>("title"); }
+            get { return Content.Value<string>("homeTitle"); }
         }
 
-        public string Exemple
+        public string HomeTopButton
         {
-            get { return Content.Value<string>("exemple"); }
+            get { return Content.Value<string>("homeTopButtom"); }
         }
 
-        //public Image Image
-        //{
-        //    get { return Content.Value<Image>("image"); }
-        //}
+        public string TitleLeft
+        {
+            get { return Content.Value<string>("titleLeft"); }
+        }
+
+        public string Sub_TitleLeft
+        {
+            get { return Content.Value<string>("sub_titleLeft"); }
+        }
+
+        public string TextLeft
+        {
+            get { return Content.Value<string>("textLeft"); }
+        }
+
+        public string LeftButton
+        {
+            get { return Content.Value<string>("leftButton"); }
+        }
+
+        public string TitleRight
+        {
+            get { return Content.Value<string>("titleRight"); }
+        }
+
+        public string Sub_TitleRight
+        {
+            get { return Content.Value<string>("sub_titleRight"); }
+        }
+
+        public string TextRight
+        {
+            get { return Content.Value<string>("textRight"); }
+        }
+
+        public string RightButton
+        {
+            get { return Content.Value<string>("rightButton"); }
+        }
+
+        public string SliderTitle
+        {
+            get { return Content.Value<string>("sliderTitle"); }
+        }
+
+        public string Slider_subTitle
+        {
+            get { return Content.Value<string>("slider_subTitle"); }
+        }
+
+        public string SliderText_1
+        {
+            get { return Content.Value<string>("sliderText_1"); }
+        }
+
+        public string Sliderauthor_1
+        {
+            get { return Content.Value<string>("sliderauthor_1"); }
+        }
+
+        public string Slidertext_2
+        {
+            get { return Content.Value<string>("slidertext_2"); }
+        }
+
+        public string Sliderauthor_2
+        {
+            get { return Content.Value<string>("sliderauthor_2"); }
+        }
+
+        public string Slidertext_3
+        {
+            get { return Content.Value<string>("slidertext_3"); }
+        }
+
+        public string Sliderauthor_3
+        {
+            get { return Content.Value<string>("sliderauthor_3"); }
+        }
+
+
+        public MvcHtmlString LearnMoreLeft
+        {
+            get { return Content.Value<MvcHtmlString>("learnMoreLeft"); }
+        }
+
+        public MvcHtmlString LearnMoreRight
+        {
+            get { return Content.Value<MvcHtmlString>("learnMoreLeft"); }
+        }
+
+        public MvcHtmlString BottomLeft
+        {
+            get { return Content.Value<MvcHtmlString>("bottomLeft"); }
+        }
+
+        public MvcHtmlString BottomCenter
+        {
+            get { return Content.Value<MvcHtmlString>("bottomCenter"); }
+        }
+
+        public MvcHtmlString BottomRight
+        {
+            get { return Content.Value<MvcHtmlString>("bottomRight"); }
+        }
+
+
 
 
         public Home(IPublishedContent content) : base(content)
         {
-               
-        }
 
+        }
 
 
     }
