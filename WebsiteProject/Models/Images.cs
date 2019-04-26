9d6f1fa6f55c593eb85256b3b28885d62af186e0
joaoborges96 <jsousaborges@hotmail.com>
2019-04-26 15:42:25 +0100

    All Site is uplouded from Umbraco

---
+++ WebsiteProject/Models/Images.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web;
+using Umbraco.Web.Models;
+
+namespace WebsiteProject.Models
+{
+    public class Images : ContentModel
+    {
+        public MvcHtmlString Image1
+        {
+            get { return Content.Value<MvcHtmlString>("image1"); }
+        }
+
+        public Images(IPublishedContent content) : base(content)
+        {
+
+        }
+    }
+}
