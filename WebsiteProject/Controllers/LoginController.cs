2c1835231b2a0d78877d256a235518bea291762e
joaoborges96 <jsousaborges@hotmail.com>
2019-05-02 13:34:23 +0100

    Register Done

---
+++ WebsiteProject/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using Umbraco.Web.Mvc;
+using WebsiteProject.Models;
+
+namespace WebsiteProject.Controllers
+{
+    public class LoginController : SurfaceController
+    {
+        [HttpGet]
+        public ActionResult Login(string hello)
+        {
+            ViewBag.hello = hello;
+            return PartialView();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Login model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.Username, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, true);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "O login está inválido");
+                }
+            }
+            return PartialView(model);
+        }
+    }
+}
---
+++ WebsiteProject/Models/Login.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public partial class RegisterModel
+    {
+        [Display(Name = "Username")]
+        [Required]
+        public string Username { get; set; }
+
+        [Display(Name = "Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Email")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Email { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
