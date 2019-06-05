7bd5d5b4ef635fa732cef95b838b99fd3b092a0a
joaoborges96 <jsousaborges@hotmail.com>
2019-06-05 15:31:48 +0100

    Alfway done

---
+++ WebsiteProject/Models/ContactChild.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteProject.Models
+{
+    public class ContactChildModel
+    {
+        [Display(Name = "Nome Completo")]
+        public string NomeCompleto { get; set; }
+
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Número de Telefone")]
+        [DataType(DataType.PhoneNumber)]
+        public int NumerodeTelefone { get; set; }
+
+        [Display(Name = "Orçamento")]
+        [DataType(DataType.Currency)]
+        public int Orcamento { get; set; }
+
+        [Display(Name = "Requisitos")]
+        public string Requisitos { get; set; }
+    }
+}
